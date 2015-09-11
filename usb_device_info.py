342c4bc2e8684f1655e51c189a0d89da4199dbce
Liam Marshall <archimedespi3141@gmail.com>
2015-09-10 20:55:26 -0500

    Wire everything up

--- usb_device_info.py
+++ usb_device_info.py
@@ -3,4 +3,13 @@
 import usb.core
 import click
 
-def 
+@click.command()
+@click.argument('vid', required=True)
+@click.argument('pid', required=True)
+def usb_device_info(vid, pid):
+	device = usb.core.find(idVendor=int(vid, 16), idProduct=int(pid, 16))
+	click.echo(device)
+	
+
+if __name__ == '__main__':
+	usb_device_info()
